e02a34e3fbede2c823eea3c15e0444b400bc0822
Facundo Laxalde Mangini <facundo.laxalde@gmail.com>
2017-05-19 20:23:49 -0300

    app android de reuniones

--- App.VecinosUY/app/src/main/java/edu/ort/vecinosuy/MeetingActivity.java
+++ App.VecinosUY/app/src/main/java/edu/ort/vecinosuy/MeetingActivity.java
@@ -1,21 +1,20 @@
 package edu.ort.vecinosuy;
 
-import android.app.DialogFragment;
-import android.content.DialogInterface;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import logic.DatePickerFragment;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import logic.Repository;
-import logic.TimePickerFragment;
-import android.app.AlertDialog.*;
-import android.app.AlertDialog;
-
-
 
 public class MeetingActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,57 +24,81 @@ public class MeetingActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_meeting);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        Button v=(Button)this.findViewById(R.id.meetingTimeBtn);
+
+        FloatingActionButton v=(FloatingActionButton)this.findViewById(R.id.addMeetingBtn);
         v.setOnClickListener(this);
-        v=(Button)this.findViewById(R.id.meetingDateBtn);
-        v.setOnClickListener(this);
-        v=(Button)this.findViewById(R.id.scheduleMeetingBtn);
-        v.setOnClickListener(this);
-    }
+
+        Bundle meetingBoundle = getIntent().getExtras();
+        ArrayList<String> meetings = new ArrayList<String>();
+        if(meetingBoundle != null) {
+            meetings = meetingBoundle.getStringArrayList("meetings");
+        }
+        final ListView listview = (ListView) findViewById(R.id.meetingListView);
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < meetings.size(); ++i) {
+            list.add(meetings.get(i));
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+               /* final String item = (String) parent.getItemAtPosition(position);
+                StringTokenizer st = new StringTokenizer(item, ":");
+                int annauncementId = Integer.parseInt(st.nextToken());
+                String announcementTitle = st.nextToken();
+                Intent i = new Intent(getApplicationContext(), AnnouncementFormActivity.class);
+                Bundle announcementsBoundle = new Bundle();
+                announcementsBoundle.putBoolean("new", false);
+                announcementsBoundle.putInt("announcementId", annauncementId);
+                announcementsBoundle.putString("announcementTitle", announcementTitle);
+                announcementsBoundle.putString("announcementImage", Repository.getInstance().announcementImage.get(annauncementId));
+                announcementsBoundle.putString("announcementBody", Repository.getInstance().announcementBody.get(annauncementId));
+                i.putExtras(announcementsBoundle);
+                startActivity(i); */
+            }
+        });
+
+
+        }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.meetingTimeBtn:
-                showTimePickerDialog(v);
-                //  finish();
-                break;
-            case R.id.meetingDateBtn:
-                showDatePickerDialog(v);
-                //  finish();
-                break;
-            case R.id.scheduleMeetingBtn:
-                schedule(v);
-                //  finish();
-                break;
-        }
-
-    }
-
-    private void schedule(View v) {
-        if (Repository.getInstance().validTimeAndDate()) {
-            // interactuar con la web api
-            finish();
-
-        } else {
-
-            new AlertDialog.Builder(this)
-                    .setTitle("Validacion de fechas")
-                    .setMessage("Ingrese fecha y hora antes de continuar")
-                    .setCancelable(true)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
+            case R.id.addMeetingBtn:
+                Intent i = new Intent(getApplicationContext(), NewMeetingActivity.class);
+                startActivity(i);
         }
     }
 
-    public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getFragmentManager(), "timePicker");
-    }
+    private class StableArrayAdapter extends ArrayAdapter<String> {
 
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getFragmentManager(), "datePicker");
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 
     @Override
