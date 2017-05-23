09daf21c30f77927e07729249f099d20bbcbd2ec
Lucía d'Avila <luda_1991@hotmail.com>
2017-05-22 21:49:36 -0300

    scroll de login y frontend android de votos

--- App.VecinosUY/app/src/main/java/edu/ort/vecinosuy/VoteActivity.java
+++ App.VecinosUY/app/src/main/java/edu/ort/vecinosuy/VoteActivity.java
@@ -1,25 +1,48 @@
 package edu.ort.vecinosuy;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+import android.app.AlertDialog.Builder;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import logic.AnnouncementContract;
 import logic.AnnouncementDbHelper;
 import logic.Repository;
+
+import static edu.ort.vecinosuy.R.string.vote;
 
 public class VoteActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -50,27 +73,47 @@ public class VoteActivity extends AppCompatActivity implements View.OnClickListe
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
-               /* final String item = (String) parent.getItemAtPosition(position);
+                final String item = (String) parent.getItemAtPosition(position);
                 StringTokenizer st = new StringTokenizer(item, ":");
-                int annauncementId = Integer.parseInt(st.nextToken());
-                String announcementTitle = st.nextToken();
-                Intent i = new Intent(getApplicationContext(), AnnouncementFormActivity.class);
-                Bundle announcementsBoundle = new Bundle();
-                announcementsBoundle.putBoolean("new", false);
-                announcementsBoundle.putInt("announcementId", annauncementId);
-                announcementsBoundle.putString("announcementTitle", announcementTitle);
-                announcementsBoundle.putString("announcementImage", Repository.getInstance().announcementImage.get(annauncementId));
-                announcementsBoundle.putString("announcementBody", Repository.getInstance().announcementBody.get(annauncementId));
-                i.putExtras(announcementsBoundle);
-                startActivity(i); */
+                final int voteId = Integer.parseInt(st.nextToken());
+
+
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                putVote(voteId+"", Repository.getInstance().votesRepository.get(voteId).EndDate,
+                                        Repository.getInstance().votesRepository.get(voteId).YesNoQuestion,
+                                        Repository.getInstance().votesRepository.get(voteId).Yes+1,
+                                        Repository.getInstance().votesRepository.get(voteId).No);
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                putVote(voteId+"", Repository.getInstance().votesRepository.get(voteId).EndDate,
+                                        Repository.getInstance().votesRepository.get(voteId).YesNoQuestion,
+                                        Repository.getInstance().votesRepository.get(voteId).Yes,
+                                        Repository.getInstance().votesRepository.get(voteId).No+1);
+                                break;
+                        }
+                    }
+                };
+                showVote(dialogClickListener, Repository.getInstance().votesRepository.get(voteId).YesNoQuestion);
             }
         });
 
 
         }
+
+    public void showVote (DialogInterface.OnClickListener dialogClickListener, String question) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("vote: " + question).setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
+    }
 
     @Override
     public void onClick(View v) {
@@ -114,5 +157,53 @@ public class VoteActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private void putVote(String voteId, String endDate, String yesNoQuestion, int yes, int no){
+        String serverAddr = getResources().getString(R.string.serverAddr) + "votes/" + voteId;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("EndDate", endDate);
+        params.put("Deleted", "false");
+        params.put("YesNoQuestion", yesNoQuestion);
+        params.put("Yes", yes+"");
+        params.put("No", no+"");
+        JsonObjectRequest request_json = new JsonObjectRequest(Request.Method.PUT, serverAddr, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        finish();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        }
+
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                String email = getLogedUserEmail();
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("TODO_PAGOS_TOKEN", email);
+
+                return params;
+            }
+        };
+        queue.add(request_json);
+        //  finish();
+    }
+    private String getLogedUserEmail(){
+        Account[] accounts = AccountManager.get(this).getAccounts();
+        Pattern emailPattern = Patterns.EMAIL_ADDRESS;
+        String possibleEmail = "";
+
+        for (Account account : accounts) {
+            if (emailPattern.matcher(account.name).matches()) {
+                possibleEmail = account.name;
+            }
+        }
+        return possibleEmail;
     }
 }
---
+++ App.VecinosUY/app/src/main/java/logic/voteDto.java
@@ -0,0 +1,16 @@
+package logic;
+
+/**
+ * Created by Facu on 5/22/17.
+ */
+
+public class voteDto {
+        public int VoteId;
+        public String EndDate;
+        public String YesNoQuestion;
+        public boolean Deleted;
+        public int Yes;
+        public int No;
+        public voteDto(){};
+
+}
